59c323e88d9b8dfffb3177530c7dae0c52eb4267
Damien Westerman <damien.westerman@snhu.edu>
2024-11-20 14:51:37 -0700

    Starting controller testing

--- src/main/java/com/damienwesterman/defensedrill/rest_api/web/CategoryController.java
+++ src/main/java/com/damienwesterman/defensedrill/rest_api/web/CategoryController.java
@@ -26,6 +26,17 @@
 
 package com.damienwesterman.defensedrill.rest_api.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/modify/category")
 public class CategoryController {
     // TODO: FINISH ME
+
+    @GetMapping
+    public void landing() {
+        
+    }
 }
---
+++ src/test/java/com/damienwesterman/defensedrill/rest_api/integration/CategoryControllerTest.java
@@ -0,0 +1,57 @@
+/****************************\
+ *      ________________      *
+ *     /  _             \     *
+ *     \   \ |\   _  \  /     *
+ *      \  / | \ / \  \/      *
+ *      /  \ | / | /  /\      *
+ *     /  _/ |/  \__ /  \     *
+ *     \________________/     *
+ *                            *
+ \****************************/
+/*
+ * Copyright 2024 Damien Westerman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.damienwesterman.defensedrill.rest_api.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.damienwesterman.defensedrill.rest_api.service.CategorySerivce;
+import com.damienwesterman.defensedrill.rest_api.web.CategoryController;
+
+@WebMvcTest(CategoryController.class)
+@AutoConfigureMockMvc
+public class CategoryControllerTest {
+    private final String CONTROLLER_ENDPOINT = "/modify/category";
+
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    CategorySerivce service;
+
+    @Test
+    public void test_test() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(
+            CONTROLLER_ENDPOINT, (Object) null))
+            .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
